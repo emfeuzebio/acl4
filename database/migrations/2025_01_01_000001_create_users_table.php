49ac18675b0210213d6a78d9b9f8b856ac2d6f93
Cap EUZEBIO <euzebio.emerson@eb.mil.br>
2025-04-15 12:13:24 -0300

    add forgot password

--- database/migrations/2025_01_01_000001_create_users_table.php
+++ database/migrations/2025_01_01_000001_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->enum('active', ['Y', 'N'])->default('Y');
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone', 18)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
--- routes/api.php
+++ routes/api.php
@@ -38,16 +38,17 @@ Route::middleware('CheckJWTToken')->group(function () {
 });
 
 // JWT Tokens
-Route::post('auth/me',          [AuthController::class, 'me']);
-Route::post('auth/login',       [AuthController::class, 'login']);
-Route::post('auth/loginSystem', [AuthController::class, 'loginSystem']);
-Route::post('auth/loginTable',  [AuthController::class, 'loginTable']);
-Route::post('auth/logout',      [AuthController::class, 'logout']);
-Route::post('auth/refresh',     [AuthController::class, 'refresh']);
-Route::post('auth/register',    [AuthController::class, 'register']);
-Route::post('auth/revoke',      [AuthController::class, 'revoke']);
-Route::post('auth/forceRefresh',[AuthController::class, 'forceRefresh']);
-Route::get( 'auth/listTokens',  [AuthController::class, 'listTokens']);
+Route::post('auth/me',            [AuthController::class, 'me']);
+Route::post('auth/login',         [AuthController::class, 'login']);
+Route::post('auth/selectSystem',  [AuthController::class, 'selectSystem']);
+Route::post('auth/forgotpassword',[AuthController::class, 'forgotpassword']);
+Route::post('auth/loginTable',    [AuthController::class, 'loginTable']);
+Route::post('auth/logout',        [AuthController::class, 'logout']);
+Route::post('auth/refresh',       [AuthController::class, 'refresh']);
+Route::post('auth/register',      [AuthController::class, 'register']);
+Route::post('auth/revoke',        [AuthController::class, 'revoke']);
+Route::post('auth/forceRefresh',  [AuthController::class, 'forceRefresh']);
+Route::get( 'auth/listTokens',    [AuthController::class, 'listTokens']);
 
 
 // JWT
