aca49feafb47488cbb23d323ad134e9878e845c0
Cap EUZEBIO <euzebio.emerson@eb.mil.br>
2025-04-15 14:48:11 -0300

    add adjust on path files

--- app/Mail/ForgotPasswordMail.php
+++ app/Mail/ForgotPasswordMail.php
@@ -21,8 +21,25 @@ class ForgotPasswordMail extends Mailable
 
     public function build()
     {
+        // com Markdown
         return $this
             ->subject('Recuperação de Senha')
-            ->markdown('emails.auth.forgot');
+            ->markdown('emails.acl.auth.forgot');
+
+        // com HTML    
+        // return $this->view('emails.auth.forgot')
+        //     ->with([
+        //         'name' => $this->name,
+        //         'url' => $this->resetUrl,
+        //     ]);            
     }
 }
+
+/*
+
+[2025-04-15 14:27:20] production.ERROR: 
+View [emails.auth.forgot] not found. {"exception":"[object] (InvalidArgumentException(code: 0): 
+    View [emails.auth.forgot] not found. at 
+    /home/u999320335/domains/fazcomphp.com.br/public_html/acl4/vendor/laravel/framework/src/Illuminate/View/FileViewFinder.php:139)
+
+*/
