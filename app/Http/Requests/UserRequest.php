843393880925e27e0e77aafee2f38326d11dfc88
Emerson Euzebio <emfeuzebio@hotmail.com>
2025-09-13 22:04:43 -0300

    unique:users,phone

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -26,7 +26,7 @@ class UserRequest extends FormRequest
             'name' => 'required|string|min:6',
             'email' => 'required|string|email|max:255|unique:users,email,' . $this->id,
             'password' => 'nullable|string|min:6|confirmed',
-            'phone' => 'string|min:10|max:15|nullable',
+            'phone' => 'string|min:10|max:15|nullable|unique:users,phone,' . $this->id,
             'photo' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
             'active' => ['required','in:"Y","N"'],
         ];
