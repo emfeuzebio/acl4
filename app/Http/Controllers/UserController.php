26a9c0f84faba1c37fe16b4f2da4df82abcddc56
Emerson Euzebio <emfeuzebio@hotmail.com>
2025-09-15 23:00:11 -0300

    Concessão de Perfil de

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -473,7 +473,8 @@ class UserController extends Controller
                 $user->profiles()->attach($request->role_id);   // assign de Role (attach)
 
                 $subject = "Concessão de Perfil de Acesso à Usuário.";
-                $text = "O Perfil de Acesso '" . $roleName . "' lhe foi Concedido com sucesso.";    // Usar assim para não quebrar Email
+                $text = htmlspecialchars("O Perfil de Acesso '{$roleName}' lhe foi Concedido com sucesso.");    // Usar assim para não quebrar Email
+                // $text = "O Perfil de Acesso \"" . $roleName . "' lhe foi Concedido com sucesso.";    // Usar assim para não quebrar Email
                 // $text = "O Perfil de Acesso '{$roleName}' lhe foi Concedido com sucesso.";
             } elseif ($request->operation == 'revokeRole') {
                 $user->profiles()->detach($request->role_id);   // Revoke the a Role (detach)
